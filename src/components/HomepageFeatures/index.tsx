f8613c0f7e696602ff9385b89bb30a5b63dd27ef
mth3860 <otkbreak@gmail.com>
2026-02-05 12:00:16 +0900

    chore: 필요없는 파일 삭제

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -10,16 +10,6 @@ type FeatureItem = {
 };
 
 const FeatureList: FeatureItem[] = [
-  {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
 ];
 
 function Feature({title, Svg, description}: FeatureItem) {
